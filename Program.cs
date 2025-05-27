1ea99907d71dadbb0ff417f0def981cbeb2c8e4d
david teller <ydt009@gmail.com>
2025-05-27 09:44:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comando_oop_exe_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
